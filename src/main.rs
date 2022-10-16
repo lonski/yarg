791012d92b4e9e00e7de9d040c9f4d81d3587bca
Michal Lonski <michal.lonski@du.co>
2022-10-16 20:52:21 +0200

    initial map; walking the player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,100 @@
-fn main() {
-    println!("Hello, world!");
+mod components;
+mod player;
+mod map;
+
+pub use map::*;
+pub use player::*;
+pub use components::*;
+
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use std::cmp::{min, max};
+use specs::prelude::*;
+
+pub struct State {
+    pub ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x, y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x, y,
+                    RGB::from_f32(0., 1.0, 0.),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'));
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Yarg")
+        .build()?;
+
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    gs.ecs.insert(new_map());
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player {})
+        .build();
+
+    rltk::main_loop(context, gs)
 }
