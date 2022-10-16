064869a92c8e73a2675087bfc1ea643f803174c5
Michal Lonski <michal.lonski@du.co>
2022-10-16 21:33:37 +0200

    map refactor

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ impl State {
     }
 }
 
-
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
@@ -29,8 +28,8 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        let map = self.ecs.fetch::<Map>();
+        draw_map(&map.tiles, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -72,17 +71,18 @@ fn draw_map(map: &[TileType], ctx: &mut Rltk) {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Yarg")
         .build()?;
+    context.with_post_scanlines(true);
 
     let mut gs = State {
         ecs: World::new()
     };
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
